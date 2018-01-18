3dca305cec59ea48e804aa3419e3408be3910d7d
Harish <Harish@DESKTOP-3I1IS3K>
2018-01-18 15:21:50 +0530

    added chromedriver

--- appjekins/src/main/java/appjekins/test.java
+++ appjekins/src/main/java/appjekins/test.java
@@ -1,13 +1,14 @@
 package appjekins;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class test {
 	
 	public void d(){
-		System.setProperty("webdriver.gecko.driver","E:\\extensions\\geckodriver.exe");
-		WebDriver driver=new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Harish\\Downloads\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		
 	}
